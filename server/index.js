7863e69ac9748158d1033a19d69b7b3a8ab23f95
khatiachub <101997849+khatiachub@users.noreply.github.com>
2023-11-07 00:08:58 +0400

    commit

--- server/index.js
+++ server/index.js
@@ -35,9 +35,9 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//   res.send("get request")
-// });
+app.get('/', (req, res) => {
+  res.send("get request")
+});
 app.post('/', (req, res) => {
   // Handle the root request here
   res.send("post request")
