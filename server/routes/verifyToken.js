b29eb4bfcbd231d97350624b70058e9c26472f71
khatiachub <101997849+khatiachub@users.noreply.github.com>
2023-11-09 00:58:22 +0400

    commit

--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -5,7 +5,7 @@ const verifyToken = (req, res, next) => {
   if (authHeader) {
     const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-      if (err) return res.status(403).json(err);
+      if (err) return res.status(401).json(err);
       req.user = user;
       next();
     });
@@ -26,11 +26,7 @@ const verifyTokenAndAdmin = async(req, res, next) => {
 };
 const verifyTokenAndAuthorization = (req, res, next) => {
   verifyToken(req, res, () => {
-    if (req.user.id === req.params.id) {
-      next();
-    } else {
-      res.status(403).json("You are not alowed to do that!");
-    }
+    next();
   });
 };
 
